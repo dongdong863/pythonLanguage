9dd8385210b471c7e7f9e1c290e8ff8b46ad6cf1
maxudong <185222324@qq.com>
2019-08-09 08:44:06 +0800

    add Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#coding:utf-8
+#dict = {key1:value1,key2:value2}
+#字典中的键必须是唯一的，值可以不唯一
+# 值可以去任何数据类型，但是键必须是不可变的，
+# 像字符窜，数字或元组这样不可变的才能是值
+# 列表不能作为字典里的键
+
+dict ={'name':'Lily','city':'beijing'}
+print(dict['na  me'])
--- tuple.py
+++ tuple.py
@@ -12,3 +12,29 @@ tuples1 =()
 print(type(tuples1))
 tuples2 =(123)
 print(type(tuples2))
+
+
+#元组拼接
+tup1 = ('apple','banana','orage')
+tup2 = ('1','2','3')
+tup3 = tup1 + tup2
+print(tup3)
+
+#删除元组后，不存在
+# tup4 = (1,2,3)
+# del tup4
+# print(tup4)
+
+#len()获取元组长度
+tup5 = (1,2,3)
+l = len(tup5)
+print(l)
+
+#遍历元组
+tup6 = ('apple','banana','orange','mango','kiwi')
+for i in tup6:
+    print(i)
+#2*(4)&(4,)*2的区别
+print(2*(4))
+print((4,)*2)
+
