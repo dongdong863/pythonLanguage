bc38cea2254439883f363f2f156bd4d5efd41cf1
maxudong <44560196+dongdong863@users.noreply.github.com>
2019-08-14 19:27:58 +0800

    modify error.py

--- error.py
+++ error.py
@@ -8,6 +8,38 @@
 #FileNotFoundError找不到文件产生的异常
 #文件夹下有read.txt，只输入read，就会报错，python并不知道read文件是说明
 # file_name = input("请输入文件名：")
-f = open(file_name)
+# f = open(file_name)
 
-#
+#处理异常
+# try：
+#     #检测范围
+# except Exception[as reson]:
+#     #出现异常后的处理
+
+# f = open('file.txt')
+# print(f.read())
+# f.close()
+#找不到文件异常
+try:
+    f= open('file.txt')
+    print(f.read())
+    f.close()
+except FileNotFoundError:
+    print('文件出错了')
+
+#0 不能做除数异常
+try:
+    num1=input('请输入被除数：')
+    num2=input('请输入除数：')
+    result = int(num1)/int(num2)
+    print(result)
+except ZeroDivisionError:
+    print('0不能做除数')
+
+#查看具体出错原因
+try:
+    f= open('file.txt')
+    print(f.read())
+    f.close()
+except FileNotFoundError as reason:
+    print('出错的原因：'+str(reason))
